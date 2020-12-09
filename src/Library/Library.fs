4d7851051154105c8d06c402e84e7cc0e106837e
Kurt Mueller <kurt.mueller@osumc.edu>
2020-12-09 10:14:30 -0500

    Basic nucleotide and dna strand logic

--- src/Library/Library.fs
+++ src/Library/Library.fs
@@ -1,5 +1,44 @@
 ﻿namespace Library
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module Domain =
+    type Nucleotide =
+        | Adenine
+        | Thymine
+        | Cytosine
+        | Guanine
+
+    module Nucleotide =
+        let complement nucleotide =
+            match nucleotide with
+            | Adenine -> Thymine
+            | Thymine -> Adenine
+            | Cytosine -> Guanine
+            | Guanine -> Cytosine
+    
+
+    type StrandDirection = 
+        | ``Five' -> Three'``
+        | ``Three' -> Five'``
+
+    type DnaStrand = {
+        Directionality: StrandDirection
+        Nucleotides: Nucleotide list
+    }
+
+    module DnaStrand =
+        let complement strand =
+            match strand.Directionality with
+            | ``Five' -> Three'`` -> { Directionality = ``Three' -> Five'``; Nucleotides = List.rev strand.Nucleotides } 
+            | ``Three' -> Five'`` -> {  Directionality = ``Five' -> Three'`` ; Nucleotides = List.rev strand.Nucleotides } 
+    
+module Input =
+    open Domain
+
+    module Nucleotide =
+        let fromCode code =
+            match code with
+            | "A" -> Ok Adenine
+            | "T" -> Ok Thymine
+            | "C" -> Ok Cytosine
+            | "G" -> Ok Guanine
+            | _ -> Error $"Not a valid nucleotide: {code}"
