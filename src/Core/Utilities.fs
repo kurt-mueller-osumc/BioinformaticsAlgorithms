9864272a72e02d6412b4c65729eb1949d9e6a493
Kurt Mueller <kurt.mueller@osumc.edu>
2021-07-28 20:48:15 +0000

    Add Results.combine utility function

--- src/Core/Utilities.fs
+++ src/Core/Utilities.fs
@@ -2,4 +2,13 @@ namespace BioinformaticsAlgorithms.Core
 
 module Utilities =
     module Results =
-        let combine = "foo"
+        /// Combine a list of results into a single result that contains either all the valid elements or a list of errors
+        let combine (results: Result<'ok, 'error> list) : Result<'ok list, 'error list> =
+            let initial : Result<'ok list, 'error list> = Ok List.empty
+
+            results
+            |> List.fold (fun aggregate result ->
+                match result with
+                | Ok ok -> aggregate |> Result.map (fun oks -> ok :: oks)
+                | Error err -> aggregate |> Result.mapError (fun errs -> err :: errs)
+            ) initial
