a4ad01808e3844dd581b533f2ac8f79014d715ef
Kurt Mueller <kurt.mueller@osumc.edu>
2022-04-14 21:01:33 +0000

    Partition results

--- src/Core/Utilities.fs
+++ src/Core/Utilities.fs
@@ -1,6 +1,7 @@
 namespace BioinformaticsAlgorithms.Core
 
 module Utilities =
+    [<RequireQualifiedAccess>]
     module Results =
         /// Combine a list of results into a single result that contains either all the valid elements or a list of errors
         let combine (results: Result<'ok, 'error> list) : Result<'ok list, 'error list> =
@@ -11,4 +12,15 @@ module Utilities =
                 match result with
                 | Ok ok -> aggregate |> Result.map (fun oks -> ok :: oks)
                 | Error err -> aggregate |> Result.mapError (fun errs -> err :: errs)
+            ) initial
+
+        /// Partition a list of results into a tuple of oks and errors
+        let partition (results: Result<'ok, 'error> list) : 'ok list * 'error list =
+            let initial : 'ok list * 'error list = ([], [])
+
+            results
+            |> List.fold (fun (oks, errors) result ->
+                match result with
+                | Ok ok -> (oks @ [ok], errors)
+                | Error err -> (oks, errors @ [err])
             ) initial
