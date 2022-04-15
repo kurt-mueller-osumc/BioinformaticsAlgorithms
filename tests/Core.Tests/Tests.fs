303dca4095b6113175ec932ff425c1d307ca7103
Kurt Mueller <kurt.mueller@osumc.edu>
2022-04-15 18:27:34 +0000

    Start to test bioinformatic algorithms

--- tests/Core.Tests/Tests.fs
+++ tests/Core.Tests/Tests.fs
@@ -3,6 +3,16 @@ module Tests
 open System
 open Xunit
 
+open BioinformaticsAlgorithms.Core
+
 [<Fact>]
 let ``My test`` () =
+    Assert.True(true)
+
+[<Fact>]
+let ``Most Frequent Words`` () =
+    let nucleotides = Nucleotides.Create "actgactcccacccc"
+
+    let mfk = nucleotides.MostFrequentKmer(3u)
+
     Assert.True(true)
